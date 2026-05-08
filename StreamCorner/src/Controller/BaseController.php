8332830b66c979ddfa4f7afb314ede1837d519cb
Raphaël <coursierap@icloud.com>
2026-05-08 08:53:04 +0200

    Refonte Connexion/Inscription + v.1 de la carte cliquable

--- StreamCorner/src/Controller/BaseController.php
+++ StreamCorner/src/Controller/BaseController.php
@@ -9,11 +9,26 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class BaseController extends AbstractController
 {
+    private const SETUP_PRODUCT_NAMES = [
+        'Command Center',
+        'Obsidian Stream Webcam',
+        'Aether Pro LED Panels',
+        'Pulse Mic',
+        'Vox Monitor Speaker',
+        'Kraken TKL Keyboard',
+    ];
+
     #[Route('/', name: 'app_accueil')]
     public function index(ProduitRepository $produitRepository): Response
     {
+        $setupProducts = [];
+        foreach ($produitRepository->findBy(['designation' => self::SETUP_PRODUCT_NAMES]) as $produit) {
+            $setupProducts[$produit->getDesignation()] = $produit;
+        }
+
         return $this->render('home/index.html.twig', [
             'featuredProducts' => $produitRepository->findBy([], ['id' => 'DESC'], 4),
+            'setupProducts' => $setupProducts,
         ]);
     }
 }
