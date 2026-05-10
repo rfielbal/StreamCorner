4027a7d2be4c0abbb475cd50a29986257591bdf9
Raphaël <coursierap@icloud.com>
2026-05-10 20:41:02 +0200

    Gestion des erreurs

--- StreamCorner/src/Controller/SecurityController.php
+++ StreamCorner/src/Controller/SecurityController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -21,12 +22,40 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'login_error' => $error instanceof AuthenticationException ? $this->loginErrorMessage($error) : null,
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    private function loginErrorMessage(AuthenticationException $error): string
+    {
+        $messageKey = $error->getMessageKey();
+        $normalizedMessage = strtolower($messageKey);
+
+        if (str_starts_with($messageKey, 'Veuillez ')) {
+            return $messageKey;
+        }
+
+        if (str_contains($normalizedMessage, 'csrf')) {
+            return 'Le formulaire de connexion a expiré. Veuillez réessayer.';
+        }
+
+        if (
+            str_contains($normalizedMessage, 'bad credentials')
+            || str_contains($normalizedMessage, 'invalid credentials')
+            || str_contains($normalizedMessage, 'password is invalid')
+            || str_contains($normalizedMessage, 'username could not be found')
+        ) {
+            return 'Email ou mot de passe incorrect.';
+        }
+
+        return 'Connexion impossible. Vérifiez vos informations puis réessayez.';
     }
 }
--- StreamCorner/src/Security/AppCustomAuthenticator.php
+++ StreamCorner/src/Security/AppCustomAuthenticator.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
@@ -28,13 +29,22 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
 
     public function authenticate(Request $request): Passport
     {
-        $email = $request->getPayload()->getString('email');
+        $email = trim($request->getPayload()->getString('email'));
+        $password = $request->getPayload()->getString('password');
+
+        if ($email === '' || $password === '') {
+            throw new CustomUserMessageAuthenticationException('Veuillez saisir votre email et votre mot de passe.');
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new CustomUserMessageAuthenticationException('Veuillez saisir une adresse email valide.');
+        }
 
         $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
 
         return new Passport(
             new UserBadge($email),
-            new PasswordCredentials($request->getPayload()->getString('password')),
+            new PasswordCredentials($password),
             [
                 new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
                 new RememberMeBadge(),
